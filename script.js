cf824eeaecbf49d7b7317af0677336b98735553d
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-05-27 22:50:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function kirimdata() {
+  var name = document.getElementById("nama").value;
+  var nim = document.getElementById("nim").value;
+  var peminatan = document.querySelector("input[name='Peminatan']:checked");
+  var alamat = document.getElementById("alamat").value;
+  var tanggal = document.getElementById("tanggal").value;
+  var angkatan = document.getElementById("angkatan").value;
+
+  if (!name || !nim || !peminatan || !alamat || !tanggal || !angkatan) {
+    alert("Semua field harus diisi!");
+    return;
+  }
+
+  alert(
+    "Nama: " +
+      name +
+      "\nNIM: " +
+      nim +
+      "\nPeminatan: " +
+      peminatan.value +
+      "\nAlamat: " +
+      alamat +
+      "\nTanggal: " +
+      tanggal +
+      "\nAngkatan: " +
+      angkatan
+  );
+}
+
+var tanggalInput = document.getElementById("tanggal");
+flatpickr(tanggalInput, {
+  enableTime: false,
+  dateFormat: "d-m-Y",
+});
